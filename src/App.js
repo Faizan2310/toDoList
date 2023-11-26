fe5e3bf43931b207d09def9f9a391537326ff167
Faizan2310 <210057917@aston.ac.uk>
2023-11-26 12:58:30 +0000

    Adding Task

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ function App() {
   const [toDoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
 
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-
+  const handleChange = (event) => setNewTask(event.target.value);
+  const addTask = () => {
+    const newToDolist = [...toDoList, newTask];
+    setTodoList(newToDolist);
   }
 
   return (
     <div className="App">
       <div className='addTask'>
         <input className='inputBox' placeholder='Insert Task' onChange={handleChange}/>
-        <button className='addBtn'>Add Task</button>
+        <button className='addBtn' onClick={addTask}>Add Task</button>
       </div>
       <div className='listTask'>
-      {newTask}
+      {toDoList.map((task)=>{return <h1>{task}</h1>})}
       </div>
     </div>
   );
